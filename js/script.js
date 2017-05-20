706303f1585ec6cb3a7411a8bd725242073e85f3
Jose Leiva <jdleiva@backcountry.com>
2017-05-19 18:38:47 -0600

    Fuentes

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var domIsReady = (function(domIsReady) {  
+  var isBrowserIeOrNot = function() {
+    return (!document.attachEvent || typeof document.attachEvent === "undefined" ? 'not-ie' : 'ie');
+  }
+  
+  domIsReady = function(callback) {
+    if(callback && typeof callback === 'function'){
+      if(isBrowserIeOrNot() !== 'ie') {
+        document.addEventListener("DOMContentLoaded", function() {          
+            return callback();
+        });
+      } else {
+        document.attachEvent("onreadystatechange", function() {
+          if(document.readyState === "complete") {
+            return callback();
+          }
+        });
+      }
+    } else {
+      console.error('The callback is not a function!');
+    }
+  }
+  
+  return domIsReady;
+})(domIsReady || {});
+
+(function(document, window, domIsReady, undefined) {
+  domIsReady(function() {
+     var questionBox = document.getElementById('question-box');
+
+     questionBox.addEventListener('click', function() {
+      if (questionBox.classList) {
+        questionBox.classList.toggle('is-open');
+      } else {
+        var classes = questionBox.className.split(' ');
+        var existingIndex = classes.indexOf('is-open');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('is-open');
+
+        questionBox.className = classes.join(' ');
+      }
+    });
+  });
+})(document, window, domIsReady);
