7a1df23d742c589f5c014aef2de459f69bc45754
Pastora Isabel Osegueda Morales <posegueda@b7-10.local>
2017-06-05 17:56:46 -0600

    agrgando cambios

--- js/script.js
+++ js/script.js
@@ -27,8 +27,9 @@ var domIsReady = (function(domIsReady) {
 (function(document, window, domIsReady, undefined) {
   domIsReady(function() {
      var questionBox = document.getElementById('question-box');
-
-     questionBox.addEventListener('click', function() {
+     var questionTitle = document.getElementById('question-title');
+     
+     questionTitle.addEventListener('click', function() {
       if (questionBox.classList) {
         questionBox.classList.toggle('is-open');
       } else {
@@ -43,5 +44,64 @@ var domIsReady = (function(domIsReady) {
         questionBox.className = classes.join(' ');
       }
     });
+
+    boxPhone = document.getElementById('box-phone');
+    inputPhone = document.getElementById('input-phone');
+
+    boxPhone.addEventListener('click', function () {
+      if (inputPhone.checked) {
+        inputPhone.classList.toggle('input-show');
+      } else {
+        var classes = inputPhone.className.split(' ');
+        var existingIndex = classes.indexOf('input-show');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('input-show');
+
+        inputPhone.className = classes.join(' ');
+      }
+    });
+
+    boxMail = document.getElementById('box-mail');
+    inputMail = document.getElementById('input-mail');
+
+    boxMail.addEventListener('click', function () {
+      if (inputMail.checked) {
+        inputMail.classList.toggle('input-show');
+      } else {
+        var classes = inputMail.className.split(' ');
+        var existingIndex = classes.indexOf('input-show');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('input-show');
+
+        inputMail.className = classes.join(' ');
+      }
+    });
+    
+    boxLetter = document.getElementById('box-letter');
+    inputLetter = document.getElementById('input-letter');
+
+    boxLetter.addEventListener('click', function () {
+      if (inputLetter.checked) {
+        inputLetter.classList.toggle('input-show');
+      } else {
+        var classes = inputLetter.className.split(' ');
+        var existingIndex = classes.indexOf('input-show');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('input-show');
+
+        inputLetter.className = classes.join(' ');
+      }
+    });
+
+
   });
 })(document, window, domIsReady);
